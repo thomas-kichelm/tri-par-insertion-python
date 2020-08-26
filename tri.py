7bf6e41a48e684d8e31dd324be095f49a4080cfc
Geutak <thomas.kichelm01@gmail.com>
2020-08-26 18:12:22 +0200

    first commit

---
+++ tri.py
@@ -0,0 +1,16 @@
+﻿import time
+
+a=[120,1,50,45]
+start=time.perf_counter()
+def tri_inser(a):
+    for i in range(1,len(a)):
+        var_aux=a[i]
+        j=i-1
+        while var_aux<a[j] and j>-1 :
+            a[j+1]=a[j]
+            j-=1
+
+        a[j+1]=var_aux
+tps=time.perf_counter()-start
+print(a)
+print(tps)
